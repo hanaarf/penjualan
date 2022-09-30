06cbe295da4d58e0faf86e8ba70a987a15b1c1ab
hanaarf <arifhana886@gmail.com>
2022-09-30 10:37:21 +0700

    nambahcss

---
+++ dashboard.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,600&display=swap" rel="stylesheet"> 
+    
+    <link rel="stylesheet" href="css/dashboard.css">
+
+</head>
+<body>
+    <div class="sidebar">
+        <div class="logo-content">
+            <div class="logo">
+                <div class="logo-name">Hollywings</div>
+            </div>
+
+
+        </div>
+
+        <ul class="nav">
+            <li>
+                <a href="#">
+                    <span class="link-name">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+        </ul>
+    </div>
+
+    <!-- <div class="home-content">
+        <div class="text">Home COntent</div>
+    </div> -->
+
+
+</body>
+</html>
---
+++ dataorder.php
@@ -0,0 +1,110 @@
+<?php
+    include 'koneksi.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Order</title>
+
+    <link rel="stylesheet" href="css/dataorder.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,600&display=swap" rel="stylesheet"> 
+
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+</head>
+<body>
+    
+<div class="smuanya">
+        <div class="kiri">
+            <div class="kiriatas">
+            <div class="logo"></div>
+                <h1>Hollywings</h1>
+            </div>
+
+            <div class="kiribawah">
+                 <a href="dashboard.php">
+                 <span class="material-symbols-outlined">
+                dashboard</span>
+                Dashboard</a> 
+                 
+                 <a href="">
+                 <span class="material-symbols-outlined">
+                event_note</span>
+                All Order</a> 
+
+                 <a href="">
+                    <span class="material-symbols-outlined">
+                        restaurant_menu</span>
+                    All Menu</a>
+
+                 <a href="">
+                    <span class="material-symbols-outlined">
+                    badge</span>
+                    Data Customer</a>
+            </div>
+        </div>
+
+        <div class="kanan">
+            <div class="kananatas">
+                <p>All Order</p>           
+                <hr>
+            </div>
+            <div class="kananbawah">
+
+
+            <center>
+            <table cellpadding="15" cellspacing="0" width = 950px >
+                <tr height = 60px>
+                    <th>No</th>
+                    <th>Nama Pelanggan</th>
+                    <th>Menu</th>
+                    <th>Harga</th>
+                    <th>Jumlah</th>
+                    <th>Action</th>
+                </tr>
+            
+
+                <?php
+                $sql = "SELECT * FROM `detail_pesan`";
+                $query = mysqli_query ($connect,$sql);
+                while($data= mysqli_fetch_array ($query)){
+                    echo "
+                <tr height = 50px class='data'>
+                    <td>$data[no]</td>
+                    <td>$data[nama_pelanggan]</td>
+                    <td>$data[menu]</td>
+                    <td>$data[harga]</td>
+                    <td>$data[jumlah]</td>
+                    <td>
+                        <a href=''>Edit </a> 
+                        <a href=''>Hapus</a>
+                    </td>
+                </tr>
+                    ";
+                }
+                ?>
+            </table>
+            </center>
+
+            </div>
+        </div>
+    </div>
+    
+
+
+</body>
+</html>
